1a06ffb0ca6526ecd053a795615918c0b9820f52
jongkook <whdrnrdl789@naver.com>
2023-11-02 17:57:34 +0900

    Vue

--- SpringBootDBMybatis/src/main/java/com/mycom/mybatis/controller/EmpController.java
+++ SpringBootDBMybatis/src/main/java/com/mycom/mybatis/controller/EmpController.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +18,7 @@ import com.mycom.mybatis.service.EmpService;
 
 // 사원 관리
 @RestController
+@CrossOrigin(origins="*", allowedHeaders = "*")
 public class EmpController {
 
 	EmpService empService;
--- SpringBootMore/src/main/java/com/mycom/more/controller/StudentController.java
+++ SpringBootMore/src/main/java/com/mycom/more/controller/StudentController.java
@@ -1,20 +1,27 @@
 package com.mycom.more.controller;
-//존잘 종국
+
 import java.util.List;
-//존잘 종국
-import org.apache.ibatis.annotations.Param;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-//존잘 종국
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
 import com.mycom.more.dto.StudentDto;
 import com.mycom.more.service.StudentService;
-//존잘 종국
+
 @RestController
+@CrossOrigin(origins="*", allowedHeaders = "*")
 public class StudentController {
-	// 존잘 종국
+
 	@Autowired
 	StudentService service;
-	// 존잘 종국
+
 	@GetMapping("/students")
 	public List<StudentDto> list(){
 		return service.list();
@@ -29,18 +36,18 @@ public class StudentController {
 	public int insert(StudentDto dto) {
 		return service.insert(dto);
 	}
-	// 존잘 종국
+
 	@PutMapping("/students/{studentId}")
 	public int update(@PathVariable int studentId ,StudentDto dto) {
 		return service.update(dto);
 	}
-	// 존잘 종국
+
 	@DeleteMapping("/students/{studentId}")
 	public int delete(@PathVariable int studentId) {
 		return service.delete(studentId);
 	}
 	
-	// 존잘 종국
+
 	@GetMapping("/students/sort")
 	public List<StudentDto> sort() {
 		return service.sort();
